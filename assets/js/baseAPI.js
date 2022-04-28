03eaab4e8f307457dbdae85f99bffbdcc11f2b84
djy <927738591@qq.com>
2022-04-28 21:27:09 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,5 +3,11 @@
 $.ajaxPrefilter(function(options){
 //在发起真正的Ajax请求之前，统一拼接请求的路径。
 options.url = 'http://www.liulongbin.top:3007' + options.url
+// 统一为有权限的接口，设置headers请求头
+if(options.url.indexOf('/my/') !== -1){
+    options.headers = {
+        Authorization: localStorage.getItem('token') || ''
+    }
+}
 console.log(options.url);
 })
